e483a4d4767d645c1315badfd7da17a0941a97d7
Chiamablessing <chiamablessing@gmail.com>
2024-07-31 05:51:18 +0100

    Updating task

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+// function that returns an array of objects who are located in a specific city
+
+export default function getStudentsByLocation(obj, city) {
+    const filterStd = obj.filter((item) => city === item.location);
+    return filterStd;
+  }
